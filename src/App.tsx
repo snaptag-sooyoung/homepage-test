3976a7bc95dd164e33af022d4f3d3b4546d5ab68
sooyoung <sooyoung159@snaptag.co.kr>
2022-08-30 18:10:28 +0900

    다운로드 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./reset.css";
+import "./App.css";
+import { UAParser } from "ua-parser-js";
+import { getDownloadURL, getStorage, ref } from "firebase/storage";
+import { storage } from "./firebase";
+import { useCallback } from "react";
 
 function App() {
+  const parser = new UAParser();
+  const result = parser.getResult();
+  console.log(result, "result");
+
+  const storage1 = storage;
+  const gsReference = ref(storage1, "gs://labcode-program.appspot.com/mac(arm)/Labcode-2.1.0-arm64.dmg");
+
+  // const downloadFile = () => {
+  //   getDownloadURL(ref(storage1, "/mac(arm)/Labcode-2.1.0-arm64.dmg"))
+  //     .then((url) => {
+  //       // `url` is the download URL for 'images/stars.jpg'
+
+  //       // This can be downloaded directly:
+  //       console.log(url);
+  //       const xhr = new XMLHttpRequest();
+  //       xhr.responseType = "blob";
+  //       xhr.onload = (event) => {
+  //         const blob = xhr.response;
+  //         console.log(blob);
+  //       };
+  //       xhr.open("GET", url);
+  //       xhr.send();
+
+  //       // Or inserted into an <img> element
+  //       // const img = document.getElementById('myimg');
+  //       // img.setAttribute('src', url);
+  //     })
+  //     .catch((error) => {
+  //       // Handle any errors
+  //     });
+  // };
+
+  const downloadFile = useCallback((os: string) => {
+    getDownloadURL(ref(storage1, "/mac(arm)/Labcode-2.1.0-arm64.dmg")).then((url) => {
+      const link = document.createElement("a");
+      document.body.appendChild(link);
+      link.href = url;
+      link.click();
+      link.remove();
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <button onClick={() => downloadFile("/mac(arm)/Labcode-2.1.0-arm64.dmg")}>{result.os.name} 다운로드</button>
     </div>
   );
 }
